9861c6f1c810e09278b53e70f9a1d558ea601c6a
Rhendydiki <rhendydikin@gmail.com>
2022-10-31 16:36:55 +0700

    Penjelasan

---
+++ Luaslingkaran.py
@@ -0,0 +1,11 @@
+#Mendeklarasikan nilai phi dari lingkaran
+phi = 3.14
+
+#Program input jari-jari lingkaran
+r = float(input("Masukkan panjang jari-jari lingkaran: "))
+
+#Rumus dari menghitung luas lingkaran
+luas = phi*r*r
+
+#Output
+print("Luas lingkaran adalah : "+ str(luas))
